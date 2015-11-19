d788c1212ab15ba6391279ce8b99438bae9cb505
星辰 <zhaozixi@kankan.com>
2015-11-19 14:30:08 +0800

    add files

---
+++ GitDemo/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  GitDemo
+//
+//  Created by 星辰 on 15/11/19.
+//  Copyright (c) 2015年 appcoda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
--- GitDemo/ViewController.h
+++ GitDemo/ViewController.h
@@ -5,11 +5,12 @@
 //  Created by 星辰 on 15/11/19.
 //  Copyright (c) 2015年 appcoda. All rights reserved.
 //
-
+#import "TestClass.h"
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
 
 @property (nonatomic)int sum;
+@property (nonatomic) TestClass *testClass;
 @end
 
